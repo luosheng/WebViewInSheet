5b1ca4f84cc225cf2260dd282f5211f1c4ebd9ef
Sheng Luo <luosheng1986@gmail.com>
2012-10-09 10:34:40 +0800

    Add LSWebPanel.

---
+++ WebViewInSheet/LSWebPanel.h
@@ -0,0 +1,13 @@
+//
+//  LSWebPanel.h
+//  WebViewInSheet
+//
+//  Created by Luo Sheng on 12-10-9.
+//  Copyright (c) 2012年 Luo Sheng. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface LSWebPanel : NSPanel
+
+@end
---
+++ WebViewInSheet/LSWebPanel.m
@@ -0,0 +1,13 @@
+//
+//  LSWebPanel.m
+//  WebViewInSheet
+//
+//  Created by Luo Sheng on 12-10-9.
+//  Copyright (c) 2012年 Luo Sheng. All rights reserved.
+//
+
+#import "LSWebPanel.h"
+
+@implementation LSWebPanel
+
+@end
