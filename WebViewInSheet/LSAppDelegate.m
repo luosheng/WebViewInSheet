d6a7c9e0b91b70e4209eb0a8234449f60cd3e1fa
Sheng Luo <luosheng1986@gmail.com>
2012-10-09 10:31:59 +0800

    Initial Commit

---
+++ WebViewInSheet/LSAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  LSAppDelegate.m
+//  WebViewInSheet
+//
+//  Created by Luo Sheng on 12-10-9.
+//  Copyright (c) 2012年 Luo Sheng. All rights reserved.
+//
+
+#import "LSAppDelegate.h"
+
+@implementation LSAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ WebViewInSheet/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  WebViewInSheet
+//
+//  Created by Luo Sheng on 12-10-9.
+//  Copyright (c) 2012年 Luo Sheng. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
